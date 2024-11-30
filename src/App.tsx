5ef57e5c2570a3225ff45e6345a420af60f92123
deV-ing08 <vintd2004@gmail.com>
2024-11-30 21:21:02 +0700

    initlize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <p className="text-red-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, explicabo!</p>
+    </div>
+  )
+}
+
+export default App
